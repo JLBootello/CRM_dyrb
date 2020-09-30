8f4d5ff97f2464dc577cecf8f054e558d6e462c5
JLBootello <64256458+JLBootello@users.noreply.github.com>
2020-09-30 05:17:20 +0200

    He creado la nueva branch mvp para trabajar en la app del mismo nombre. Esta app es la antigua app_representada y en ella estamos incluyendo todos los forms de los models que tenemos creados hasta ahora.

--- django_crm_dyrb/mvp/urls.py
+++ django_crm_dyrb/mvp/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('nuevarepresentada', views.RepresentadaView.as_view(), name='nueva_representada'),
-    path('listarepresentadas', views.RepresentadaUpdateView.as_view(), name='lista_representadas'),
+    path('listarepresentadas', views.RepresentadaListView.as_view(), name='lista_representadas'),
+    path('editarepresentadas/<int:pk>', views.RepresentadaUpdateView.as_view(), name='edita_representadas'),
     path('contactorepresentada', views.ContactoRepresentadaView.as_view(), name='contacto_representada'),
     path('listacontactosrepresentadas', views.ContactoRepresentadaUpdateView.as_view(), name='lista_contactos_representadas'),
     path('clienterepresentada', views.ClienteRepresentadaView.as_view(), name='cliente_representada'),
